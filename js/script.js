e50252cb586d2e9a1652838e64d87a16f64c048e
Mark <thekeesie@hotmail.com>
2018-11-18 20:17:11 +0100

    Version 3.0
    - Blok 3 toegevoegd
    - Font, knoppen en checkboxes mooier gemaakt
    - Bug dat je niks kan aanvinken gefixt
    - Optie om foto te refreshen
    - Popup tekst veranderd
    - Baardagaam3 JPG to jpg

--- js/script.js
+++ js/script.js
@@ -5,23 +5,36 @@ var Schapen = ["Swifter", "Rackaschaap", "Texelaar", "Coburger Fuchs", "Hampshir
 var Runden = ["Lakenvelder", "Witrik", "Groninger Blaarkop", "Maas-Rijn-IJssel", "Holstein-Friesian", "Yersey", "Dexter", "Zeboe", "Belgische Blauwe", "Limousin", "Aberdeen Angus", "Schotse Hooglander"]
 var Geiten = ["Angorageit", "Anglo-Nubische Geit", "Afrikaanse Boergeit", "West-Afrikaanse Dwerggeit", "Belgische Hertegeit", "Nederlandse Landgeit", "Nederlandse Bonte Geit", "Toggenburger", "Nederlandse Witte Geit", "Wallische Zwarthalsgeit"];
 var Overige = ["Alpaca", "Lama", "Damhert"];
-var PaardenBackup = Paarden.slice()
-var VarkensBackup = Varkens.slice()
-var SchapenBackup = Schapen.slice()
-var RundenBackup = Runden.slice()
-var GeitenBackup = Geiten.slice()
-var OverigeBackup = Overige.slice()
+var PaardenBackup = Paarden.slice();
+var VarkensBackup = Varkens.slice();
+var SchapenBackup = Schapen.slice();
+var RundenBackup = Runden.slice();
+var GeitenBackup = Geiten.slice();
+var OverigeBackup = Overige.slice();
 
 //Dieren blok 2//
 var Konijnen = ["Angora", "Belgische Haas", "Duitse Hangoor", "Eksterkonijn", "Engelse Hangoor", "Franse Hangoor", "Groot Chinchilla", "Groot Lotharinger", "Hollander", "Japanner", "Kleurdwerg", "Nederlandse Hangoordwerg", "Papillon", "Pool", "Rex", "Rijnlander", "Rus", "Satijn", "Tan", "Thrianta", "Vlaamse Reus", "Voskonijn", "Witte Nieuw Zeelander", "Witte Van Hotot"];
 var Knaagdieren = ["Cavia Borstelhaar", "Cavia Gladhaar", "Cavia Rex", "Cavia Satijn", "Cavia Sheltie", "Cavia Amerikaanse Gekruind", "Cavia Tessel", "Chinchilla", "Degoe Wildkleur", "Dikstaartgerbil", "Kleurmuis", "Mongoolse Gerbil", "Roborovski Dwerghamster", "Russische Dwerghamster", "Syrische Hamster", "Tamme Rat", "Chinese Dwerghamster", "Boeroendoek", "Sugarglider", "Japanse Eekhoorn", "Veeltepelmuis"];
 var Herpeten = ["Griekse Landschildpad", "Roodwangschildpad", "Geelwangschildpad", "Rode Rattenslang", "Koningspython", "Afgodslang", "Groene Leguaan", "Baardagaam", "Luipaardgekko", "Pijlgifkikker", "Vuurbuikpadje", "Mexicaanse Roodknievogelspin"];
 var Vissen = ["Goudvis", "Sluierstaart", "Koi", "Goudwinde", "Guppy", "Zwaarddrager", "Platy", "Black Mollie", "Neontetra", "Kardinaaltetra", "Sumatraan", "Maanvis", "Discusvis", "Kempvis", "Pantsermeerval"];
-var DierenBlok2 = ["Konijnen", "Knaagdieren", "Herpeten", "Vissen"]
-var KonijnenBackup = Konijnen.slice()
-var KnaagdierenBackup = Knaagdieren.slice()
-var HerpetenBackup = Herpeten.slice()
-var VissenBackup = Vissen.slice()
+var DierenBlok2 = ["Konijnen", "Knaagdieren", "Herpeten", "Vissen"];
+var KonijnenBackup = Konijnen.slice();
+var KnaagdierenBackup = Knaagdieren.slice();
+var HerpetenBackup = Herpeten.slice();
+var VissenBackup = Vissen.slice();
+
+
+//Dieren blok 3//
+var Volierevogels = ["Kanarie", "Zebravink", "Japanse Meeuw", "Diamantduif", "Chinese Dwergkwartel", "Grasparkiet", "Valkparkiet", "Agapornis Roseicollis", "Agapornis Personata", "Agapornis Fischeri", "Rijstvogel", "Mozambiquesijs", "Roodrugparkiet", "Grijze Roodstaartpapegaai", "Prachtrosella", "Halsbandparkiet", "Bont Boertje", "Lori van de Blauwe Bergen", "Lachduif", "Diamantvink", "Spitsstaartamadine", "Californische Kuifkwartel", "Japanse Kwartel", "Mexicaanse Roodmus", "Gouldsamadine", "Binsenastrilde", "Sijs", "Goudvink", "Putter", "Vink", "Blauwgele Ara", "Geelkuifkaketoe", "Blauwvoorhoofdamazone"];
+var Duiven = ["Nonduif", "Oud-Hollandse Kapucijn", "Oud-Hollande Meeuw", "Indiase Pauwstaart", "Kingduif", "Hollandse Kropper", "Turkse Tortel"];
+var Hoenders = ["Antwerpse Baardkriel", "Araucana", "Barnevelder", "Brahma", "Chabo", "Hollandse Kriel", "Hollandse Kuifhoen", "Javakriel", "Lakenvelder", "Mergellandhoen", "Nederlandse Sebelpootkriel", "Orpington", "Sebright", "Vorwerkhoen", "Wyandotte", "Zijdehoen"];
+var Watervogels = ["Mandarijneend", "Carolina Eend", "Muskeseend", "Kuifeend", "Wilde Eend", "Indische Loopeend", "Kwaker", "Duitse Pekingeend", "Tamme Kuifeend", "Zwarte Zwaan", "Knobbelzwaan", "Afrikaanse Knobbelgans", "Nijlgans", "Canadese Gans", "Brandgans", "Grauwe Gans", "Toulouse Gans", "Landgans"];
+var Siervogels = ["Blauwe Pauw", "Bosfazant", "Goudfazant", "Bankivahoen", "Kalkoen", "Parelhoen"];
+var VolierevogelsBackup = Volierevogels.slice();
+var DuivenBackup = Duiven.slice();
+var HoendersBackup = Hoenders.slice();
+var WatervogelsBackup = Watervogels.slice();
+var SiervogelsBackup = Siervogels.slice();
 
 //Overig//
 var huidigBlok = 0;
@@ -32,6 +45,7 @@ var score = 0;
 var randomDier = "";
 var totaalPunten = 0;
 var Dieren = [];
+var randomGetal = 0;
 
 //Functions//
 
@@ -39,7 +53,7 @@ function checkAntwoord()
 {
     hideGoedAntwoord();
     gebruikersAntwoord = $('#antwoordInput').val();
-    if (goedAntwoord === "Tinker" && gebruikersAntwoord === "zul"){
+    if (gebruikersAntwoord === "zul"){
         console.log("lol");
         $('body').css('background-image', 'url(./images/random/zul1.jpg)');
         document.getElementById('foto').src = "./images/random/zul1.jpg";
@@ -48,7 +62,7 @@ function checkAntwoord()
         return
     }
 
-    if (goedAntwoord === "Lama" && gebruikersAntwoord === "fortnite"){
+    if (gebruikersAntwoord === "fortnite"){
         console.log("FORTNITE");
         $('body').css('background-image', 'url(./images/random/fortnite.gif)');
         document.getElementById('foto').src = "./images/random/fortnite.jpg";
@@ -113,100 +127,109 @@ function checkAntwoord2(){
 function getFoto()
 {
         randomDier = Dieren[Math.floor(Math.random() * Dieren.length)];
-        var randomGetal = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+        randomGetal = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
         console.log(randomDier);
-        if (randomDier == "Paarden" && Paarden.length !== 0) {
+        if (randomDier === "Paarden" && Paarden.length !== 0) {
             var randomPaard = Paarden[Math.floor(Math.random() * Paarden.length)];
-            //while (vragenKlaar.includes(randomPaard)) {
-            //    randomPaard = Paarden[Math.floor(Math.random() * Paarden.length)];
-            //}
             console.log(randomPaard)
             document.getElementById('foto').src = './images/' + randomDier + '/' + randomPaard + '/' + randomPaard + randomGetal + '.jpg';
             goedAntwoord = randomPaard;
-        } else if (randomDier == "Varkens" && Varkens.length !== 0) {
+        } else if (randomDier === "Varkens" && Varkens.length !== 0) {
             var randomVarken = Varkens[Math.floor(Math.random() * Varkens.length)];
-            //while (vragenKlaar.includes(randomVarken)) {
-            //    randomVarken = Varkens[Math.floor(Math.random() * Varkens.length)];
-            //}
             console.log(randomVarken)
             document.getElementById('foto').src = './images/' + randomDier + '/' + randomVarken + '/' + randomVarken + randomGetal + '.jpg';
             goedAntwoord = randomVarken;
 
-        } else if (randomDier == "Schapen" && Schapen.length !== 0) {
+        } else if (randomDier === "Schapen" && Schapen.length !== 0) {
             var randomSchaap = Schapen[Math.floor(Math.random() * Schapen.length)];
-            //while (vragenKlaar.includes(randomSchaap)) {
-            //    randomSchaap = Schapen[Math.floor(Math.random() * Schapen.length)];
-            //}
-            console.log(randomSchaap)
+            console.log(randomSchaap);
             document.getElementById('foto').src = './images/' + randomDier + '/' + randomSchaap + '/' + randomSchaap + randomGetal + '.jpg';
             goedAntwoord = randomSchaap;
 
-        } else if (randomDier == "Runden" && Runden.length !== 0) {
+        } else if (randomDier === "Runden" && Runden.length !== 0) {
             var randomRund = Runden[Math.floor(Math.random() * Runden.length)];
-            //while (vragenKlaar.includes(randomRund)) {
-            //    randomRund = Runden[Math.floor(Math.random() * Runden.length)];
-            //}
-            console.log(randomRund)
+            console.log(randomRund);
             document.getElementById('foto').src = './images/' + randomDier + '/' + randomRund + '/' + randomRund + randomGetal + '.jpg';
             goedAntwoord = randomRund;
 
-        } else if (randomDier == "Geiten" && Geiten.length !== 0) {
+        } else if (randomDier === "Geiten" && Geiten.length !== 0) {
             var randomGeit = Geiten[Math.floor(Math.random() * Geiten.length)];
-            //while (vragenKlaar.includes(randomGeit)) {
-            //    randomGeit = Geiten[Math.floor(Math.random() * Geiten.length)];
-            //}
-            console.log(randomGeit)
+            console.log(randomGeit);
             document.getElementById('foto').src = './images/' + randomDier + '/' + randomGeit + '/' + randomGeit + randomGetal + '.jpg';
             goedAntwoord = randomGeit;
 
-        } else if (randomDier == "Overige" && Overige.length !== 0) {
+        } else if (randomDier === "Overige" && Overige.length !== 0) {
             var randomOverige = Overige[Math.floor(Math.random() * Overige.length)];
-            //while (vragenKlaar.includes(randomOverige)) {
-            //    randomOverige = Overige[Math.floor(Math.random() * Overige.length)];
-            //}
-            console.log(randomOverige)
+            console.log(randomOverige);
             document.getElementById('foto').src = './images/' + randomDier + '/' + randomOverige + '/' + randomOverige + randomGetal + '.jpg';
             goedAntwoord = randomOverige;
 
-        } else if (randomDier == "Konijnen" && Konijnen.length !== 0) {
+        } else if (randomDier === "Konijnen" && Konijnen.length !== 0) {
             var randomKonijnen = Konijnen[Math.floor(Math.random() * Konijnen.length)];
-            //while (vragenKlaar.includes(randomKonijnen)) {
-            //    randomKonijnen = Konijnen[Math.floor(Math.random() * Konijnen.length)];
-            //}
-            console.log(randomKonijnen)
+            console.log(randomKonijnen);
             document.getElementById('foto').src = './images/' + randomDier + '/' + randomKonijnen + '/' + randomKonijnen + randomGetal + '.jpg';
             goedAntwoord = randomKonijnen;
 
-        } else if (randomDier == "Knaagdieren" && Knaagdieren.length !== 0) {
+        } else if (randomDier === "Knaagdieren" && Knaagdieren.length !== 0) {
             var randomKnaagdieren = Knaagdieren[Math.floor(Math.random() * Knaagdieren.length)];
-            //while (vragenKlaar.includes(randomKnaagdieren)) {
-            //    randomKnaagdieren = Knaagdieren[Math.floor(Math.random() * Knaagdieren.length)];
-            //}
-            console.log(randomKnaagdieren)
+            console.log(randomKnaagdieren);
             document.getElementById('foto').src = './images/' + randomDier + '/' + randomKnaagdieren + '/' + randomKnaagdieren + randomGetal + '.jpg';
             goedAntwoord = randomKnaagdieren;
 
-        } else if (randomDier == "Herpeten" && Herpeten.length !== 0) {
+        } else if (randomDier === "Herpeten" && Herpeten.length !== 0) {
             var randomHerpeten = Herpeten[Math.floor(Math.random() * Herpeten.length)];
-            //while (vragenKlaar.includes(randomHerpeten)) {
-            //    randomHerpeten = Herpeten[Math.floor(Math.random() * Herpeten.length)];
-            //}
-            console.log(randomHerpeten)
+            console.log(randomHerpeten);
             document.getElementById('foto').src = './images/' + randomDier + '/' + randomHerpeten + '/' + randomHerpeten + randomGetal + '.jpg';
             goedAntwoord = randomHerpeten;
 
-        } else if (randomDier == "Vissen" && Vissen.length !== 0) {
+        } else if (randomDier === "Vissen" && Vissen.length !== 0) {
             var randomVissen = Vissen[Math.floor(Math.random() * Vissen.length)];
-            //while (vragenKlaar.includes(randomVissen)) {
-            //    randomVissen = Vissen[Math.floor(Math.random() * Vissen.length)];
-            //}
-            console.log(randomVissen)
+            console.log(randomVissen);
             document.getElementById('foto').src = './images/' + randomDier + '/' + randomVissen + '/' + randomVissen + randomGetal + '.jpg';
             goedAntwoord = randomVissen;
+
+        } else if (randomDier === "Volierevogels" && Volierevogels.length !== 0) {
+            var randomVolierevogels = Volierevogels[Math.floor(Math.random() * Volierevogels.length)];
+            console.log(randomVolierevogels);
+            document.getElementById('foto').src = './images/' + randomDier + '/' + randomVolierevogels + '/' + randomVolierevogels + randomGetal + '.jpg';
+            goedAntwoord = randomVolierevogels;
+
+        } else if (randomDier === "Duiven" && Duiven.length !== 0) {
+            var randomDuiven = Duiven[Math.floor(Math.random() * Duiven.length)];
+            console.log(randomDuiven);
+            document.getElementById('foto').src = './images/' + randomDier + '/' + randomDuiven + '/' + randomDuiven + randomGetal + '.jpg';
+            goedAntwoord = randomDuiven;
+
+        } else if (randomDier === "Hoenders" && Hoenders.length !== 0) {
+            var randomHoenders = Hoenders[Math.floor(Math.random() * Hoenders.length)];
+            console.log(randomHoenders);
+            document.getElementById('foto').src = './images/' + randomDier + '/' + randomHoenders + '/' + randomHoenders + randomGetal + '.jpg';
+            goedAntwoord = randomHoenders;
+
+        } else if (randomDier === "Watervogels" && Watervogels.length !== 0) {
+            var randomWatervogels = Watervogels[Math.floor(Math.random() * Watervogels.length)];
+            console.log(randomWatervogels);
+            document.getElementById('foto').src = './images/' + randomDier + '/' + randomWatervogels + '/' + randomWatervogels + randomGetal + '.jpg';
+            goedAntwoord = randomWatervogels;
+
+        } else if (randomDier === "Siervogels" && Siervogels.length !== 0) {
+            var randomSiervogels = Siervogels[Math.floor(Math.random() * Siervogels.length)];
+            console.log(randomSiervogels);
+            document.getElementById('foto').src = './images/' + randomDier + '/' + randomSiervogels + '/' + randomSiervogels + randomGetal + '.jpg';
+            goedAntwoord = randomSiervogels;
 
         } else {
             checkCompleet();
         }
+};
+
+function refreshFoto(){
+    var rand = randomGetal;
+    while (rand === randomGetal){
+        rand = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+    }
+    randomGetal = rand;
+    document.getElementById('foto').src = './images/' + randomDier + '/' + goedAntwoord + '/' + goedAntwoord + rand + '.jpg';
 };
 
 function pushKlaar(){
@@ -244,6 +267,17 @@ function selectBlok2()
     huidigBlok = 2;
 };
 
+function selectBlok3()
+{
+    $(".radio").css('display') == 'block'
+    $(".radio").show();
+    $("#radioDierenBlok3").css('display') == 'block'
+    $("#radioDierenBlok3").show();
+    $(".selectBlok").css('display') == 'none'
+    $(".selectBlok").hide();
+    huidigBlok = 3;
+};
+
 function checkboxToArray() {
     var form = document.getElementById("radioDierenBlok" + huidigBlok),
         inputs = form.getElementsByTagName("input");
@@ -257,24 +291,21 @@ function checkboxToArray() {
 
 function startQuiz(){
     checkboxToArray();
-    $(".radio").css('display') == 'none'
-    $(".radio").hide();
-    $(".selectBlok").css('display') == 'none'
-    $(".selectBlok").hide();
-    $("#radioDierenBlok1").css('display') == 'none'
-    $("#radioDierenBlok1").hide();
-    $("#radioDierenBlok2").css('display') == 'none'
-    $("#radioDierenBlok2").hide();
-    $(".radio").css('display') == 'none'
-    $(".radio").hide();
-    $(".main").css('display') == 'block'
-    $(".main").show();
 
-    for (var i=0 ; i<Dieren.length;i++){
-        totaalPunten += window[Dieren[i]].length;
+    if (Dieren.length <= 0){
+        window.alert("Je moet wel iets selecteren, eikelbrein.")
+    } else {
+        $(".radio").css('display') == 'none'
+        $(".radio").hide();
+        $(".main").css('display') == 'block'
+        $(".main").show();
+
+        for (var i = 0; i < Dieren.length; i++) {
+            totaalPunten += window[Dieren[i]].length;
+        }
+        $('#totaalPunten').html(totaalPunten);
+        getFoto();
     }
-    $('#totaalPunten').html(totaalPunten);
-    getFoto();
 }
 
 function showCheck(){
@@ -365,6 +396,8 @@ function goBack(){
     $("#radioDierenBlok1").hide();
     $("#radioDierenBlok2").css('display') == 'none'
     $("#radioDierenBlok2").hide();
+    $("#radioDierenBlok3").css('display') == 'none'
+    $("#radioDierenBlok3").hide();
     $(".radio").css('display') == 'none'
     $(".radio").hide();
     $(".selectBlok").css('display') == 'block'
